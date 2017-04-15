f9fcbb2518ce8d49c01e41453cd0d39ef938838c
wanyu <wanyu@wirelessprv-10-193-103-138.near.illinois.edu>
2017-04-15 00:07:25 -0500

    fixed math

--- temp/myTimeInputTable.php
+++ temp/myTimeInputTable.php
@@ -11,7 +11,7 @@ class MyDB extends SQLite3{
 
 $dir = 'sqlite:/Users/wanyu/Desktop/CS196/MyTime/temp/cs196.sqlite';
 $dbh = new PDO($dir) or die ("cannot open the database");
-$query = "INSERT INTO CS1962 ('TASK', 'TAG', 'DURATION', 'DATE')
+$query = "INSERT INTO CS1962 (TASK, TAG, DURATION, DATE)
 VALUES
 (3, '$_POST[task]','$_POST[tag]', $_POST[duration],'$_POST[date]')";
 echo " record added";
